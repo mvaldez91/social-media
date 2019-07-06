3f60e9e410e11b9e8442f575ca16ecdec8dadcf7
marco <mtulio1991@live.com>
2019-07-06 11:17:45 -0600

    Add config file

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+module.exports.CONFIG = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+
+module.exports.LANG = 'EN';
